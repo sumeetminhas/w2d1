09aed5e8327250f745622c97219ed5fd444fcbc3
Sumeet Minhas <sumeetm273@gmail.com>
2017-03-13 19:47:19 +0000

    first commit

---
+++ step3.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+
+  https.get(options, function(response) {
+
+    var stream = "";
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(chunk) {
+      console.log(chunk);
+      console.log('Chunk recieved.' + '\n');
+      stream = stream + chunk;
+    });
+
+    response.on('end', function() {
+      console.log(stream);
+      console.log('Response stream complete.');
+    });
+  });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
